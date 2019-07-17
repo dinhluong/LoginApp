b5096cf54880bdb6aa87163f1432084b93b5fd4f
Nguyen Tien Tuyen <30790987+tuyenguyen24@users.noreply.github.com>
2019-07-17 22:22:56 +0900

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_GkZRI3o96', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-northeast-1', // e.g. us-east-2
+		clientId: '2arki0048c0j4tieeqjssa91jj' //is this used anywhere?
+    },
+};
+
